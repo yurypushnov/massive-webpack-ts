822a2b6971d0f704a88f70ca2f5be988fca12bde
Yury Pushnov <yury.pushnov@principleglobal.com>
2019-10-18 12:31:25 +0300

    Fix MassiveJS + Webpack problem with copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -19,10 +20,16 @@ module.exports = {
   },
   plugins: [
     new webpack.IgnorePlugin(/\.\/native/, /\/pg\//),
+    new CopyPlugin([
+      { from: 'node_modules/massive/lib/scripts', to: 'scripts' },
+    ]),
   ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   target: 'node',
+  node: {
+    __dirname: false,
+  }
 };
